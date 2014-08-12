8716bb7554a9bb6d0a995d69d1f87a5277b193d0
77web <info@77-web.com>
2014-08-13 08:48:33 +0900

    change ServiceFactory to have core services

--- src/ServiceFactory.php
+++ src/ServiceFactory.php
@@ -11,6 +11,19 @@ namespace Excel\Templating;
  */
 class ServiceFactory
 {
+    const COLUMN_CONCEALER = 'column_concealer';
+    const COLUMN_REMOVER = 'column_remover';
+    const RENDERER = 'renderer';
+    const ROW_CONCEALER = 'row_concealer';
+    const ROW_REMOVER = 'row_remover';
+    const SHEET_REMOVER = 'sheet_remover';
+
+    /**
+     * @var array
+     * an assoc with service name => FQCN
+     */
+    private static $coreServiceList;
+
     /**
      * @var array
      * an assoc with service name => FQCN
@@ -20,9 +33,26 @@ class ServiceFactory
     /**
      * @param array $serviceList
      */
-    public function __construct(array $serviceList = null)
+    public function __construct(array $serviceList = [])
     {
-        $this->availableServices = $serviceList;
+        self::initializeCoreServices();
+
+        $this->availableServices = array_merge(self::$coreServiceList, $serviceList);
+    }
+
+    /**
+     * initialize core service
+     */
+    private static function initializeCoreServices()
+    {
+        self::$coreServiceList = [
+            self::COLUMN_CONCEALER => '\Excel\Templating\Service\ColumnConcealer',
+            self::COLUMN_REMOVER => '\Excel\Templating\Service\ColumnRemover',
+            self::RENDERER => '\Excel\Templating\Service\Renderer',
+            self::ROW_CONCEALER => '\Excel\Templating\Service\RowConcealer',
+            self::ROW_REMOVER => '\Excel\Templating\Service\RowRemover',
+            self::SHEET_REMOVER => '\Excel\Templating\Service\SheetRemover',
+        ];
     }
 
     /**
--- tests/ServiceFactoryTest.php
+++ tests/ServiceFactoryTest.php
@@ -15,6 +15,22 @@ class ServiceFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\stdClass', $testService);
     }
 
+    public function test_create_core()
+    {
+        $factory = new ServiceFactory();
+        $rendererService = $factory->create('renderer');
+
+        $this->assertInstanceOf('\Excel\Templating\Service\Renderer', $rendererService);
+    }
+
+    public function core_service_can_be_overriden_by_constructor()
+    {
+        $factory = new ServiceFactory([ServiceFactory::RENDERER => '\stdClass']);
+        $service = $factory->create(ServiceFactory::RENDERER);
+
+        $this->assertInstanceOf('\stdClass', $service);
+    }
+
     /**
      * @expectedException \LogicException
      */
